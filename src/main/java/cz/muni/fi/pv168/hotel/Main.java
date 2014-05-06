e5f90691a70940908deb0474e045b4453cf95b27
Andrej Kuročenko <andrej@kurochenko.net>
2014-05-06 17:02:03 +0200

    commented out meaningless configguration

--- src/main/java/cz/muni/fi/pv168/hotel/Main.java
+++ src/main/java/cz/muni/fi/pv168/hotel/Main.java
@@ -1,20 +1,8 @@
 package cz.muni.fi.pv168.hotel;
 
-import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.io.IOException;
@@ -32,7 +20,7 @@ public class Main {
 //
 //        PersonManager personManager = new PersonManagerImpl(db);
 
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+//        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
         //RoomManager roomManager = ctx.getBean(RoomManager.class);
 
 //        RoomManager roomManager = new RoomManagerImpl(getDateSource());
@@ -45,58 +33,58 @@ public class Main {
 //        roomManager.createRoom(room);
     }
 
-    static String readFile(String path) {
-        return convertStreamToString(Main.class.getClassLoader().getResourceAsStream(path));
-    }
+//    static String readFile(String path) {
+//        return convertStreamToString(Main.class.getClassLoader().getResourceAsStream(path));
+//    }
+//
+//    static String convertStreamToString(java.io.InputStream is) {
+//        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+//        return s.hasNext() ? s.next() : "";
+//    }
+//
+//    @Configuration
+//    @EnableTransactionManagement
+//    @PropertySource("classpath:myconf.properties")
+//    public static class SpringConfig {
+//
+//        @Autowired
+//        Environment env;
+//
+//        @Bean
+//        public DataSource dataSource() {
+//            BasicDataSource bds = new BasicDataSource(); //Apache DBCP connection pooling DataSource
+//            bds.setUrl(env.getProperty("jdbc.url"));
+//            bds.setDriverClassName("org.apache.derby.jdbc.ClientDriver");
+//            bds.setUsername(env.getProperty("jdbc.user"));
+//            bds.setPassword(env.getProperty("jdbc.password"));
+//            return bds;
+//        }
+//
+//        @Bean
+//        public PlatformTransactionManager transactionManager() {
+//            return new DataSourceTransactionManager(dataSource());
+//        }
+//
+//        @Bean
+//        public RoomManager roomManager() {
+//            return new RoomManagerImpl(dataSource());
+//        }
+//
+//        @Bean
+//        public PersonManager personManager() {
+//            return new PersonManagerImpl(new TransactionAwareDataSourceProxy(dataSource()));
+//        }
+//
+//        @Bean
+//        public RentManager rentManager() {
+//            RentManagerImpl rentManager = new RentManagerImpl(dataSource());
+//            rentManager.setRoomManager(roomManager());
+//            rentManager.setPersonManager(personManager());
+//            return rentManager;
+//        }
+//    }
 
-    static String convertStreamToString(java.io.InputStream is) {
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
-    }
-
-    @Configuration
-    @EnableTransactionManagement
-    @PropertySource("classpath:myconf.properties")
-    public static class SpringConfig {
-
-        @Autowired
-        Environment env;
-
-        @Bean
-        public DataSource dataSource() {
-            BasicDataSource bds = new BasicDataSource(); //Apache DBCP connection pooling DataSource
-            bds.setUrl(env.getProperty("jdbc.url"));
-            bds.setDriverClassName("org.apache.derby.jdbc.ClientDriver");
-            bds.setUsername(env.getProperty("jdbc.user"));
-            bds.setPassword(env.getProperty("jdbc.password"));
-            return bds;
-        }
-
-        @Bean
-        public PlatformTransactionManager transactionManager() {
-            return new DataSourceTransactionManager(dataSource());
-        }
-
-        @Bean
-        public RoomManager roomManager() {
-            return new RoomManagerImpl(dataSource());
-        }
-
-        @Bean
-        public PersonManager personManager() {
-            return new PersonManagerImpl(new TransactionAwareDataSourceProxy(dataSource()));
-        }
-
-        @Bean
-        public RentManager rentManager() {
-            RentManagerImpl rentManager = new RentManagerImpl(dataSource());
-            rentManager.setRoomManager(roomManager());
-            rentManager.setPersonManager(personManager());
-            return rentManager;
-        }
-    }
-
-    /*public static DataSource getDateSource(){
+    public static DataSource getDateSource(){
 //        Properties myConf = new Properties();
 //
 //        try {
@@ -117,6 +105,6 @@ public class Main {
 
         return build;
     }
-*/
+
     //ahoj
 }
