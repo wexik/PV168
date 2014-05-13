376d9b00af7fde00de51bd6c0e45eef55a0651f8
Andrej Kuročenko <andrej@kurochenko.net>
2014-05-13 23:15:20 +0200

    created rent form

---
+++ src/main/java/cz/muni/fi/pv168/hotel/gui/RentForm.java
@@ -0,0 +1,26 @@
+package cz.muni.fi.pv168.hotel.gui;
+
+import javax.swing.*;
+
+/**
+ * @author kurocenko
+ */
+public class RentForm extends JPanel {
+    private JPanel panel1;
+    private JButton cancelButton;
+    private JLabel roomabel;
+    private JLabel personLabel;
+    private JLabel startLabel;
+    private JLabel endLabel;
+    private JLabel numberLabel;
+    private JComboBox roomCombo;
+    private JComboBox personCombo;
+    private JComboBox numberCombo;
+    private JTextField startField;
+    private JTextField endField;
+    private JTable rentTable;
+    private JButton deleteButton;
+    private JButton updateButton;
+    private JButton okButton;
+    private JButton closeButton;
+}
