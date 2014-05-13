d30d9c2496445944b3c27cc5d5de361165841085
Andrej Kuročenko <andrej@kurochenko.net>
2014-05-13 21:07:19 +0200

    room form added

---
+++ src/main/java/cz/muni/fi/pv168/hotel/gui/RoomForm.java
@@ -0,0 +1,20 @@
+package cz.muni.fi.pv168.hotel.gui;
+
+import javax.swing.*;
+
+/**
+ * @author kurocenko
+ */
+public class RoomForm {
+    private JTextField capacityField;
+    private JTextField priceField;
+    private JLabel capacityLabel;
+    private JPanel priceLabel;
+    private JButton cancelButton;
+    private JButton okButton;
+    private JTextField numberField;
+    private JLabel numberLabel;
+    private JTable roomTable;
+    private JButton deleteButton;
+    private JButton updateButton;
+}
